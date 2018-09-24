3b641c85359e8b7f4de3d59cd728fcf4aeab7118
Jose Velez <josevelezq7@gmail.com>
2018-09-23 23:23:20 -0400

    Committing lots of changes. All sections of the website are done. Added sections Products and Projects. Changed layout. Added HTML, CSS, images, etc.

--- scripts/googlemaps.js
+++ scripts/googlemaps.js
@@ -1,5 +1,5 @@
 function myMap() {
-    var myCenter = new google.maps.LatLng(18.403479,-66.056622);
+    var myCenter = new google.maps.LatLng(18.390070,-66.095146);
     var mapCanvas = document.getElementById("googleMap");
     var mapOptions = {center: myCenter, zoom: 14, mapTypeControl: true,
         mapTypeControlOptions: {
@@ -11,7 +11,7 @@ function myMap() {
     marker.setMap(map);
     google.maps.event.addListener(marker,'click',function() {
         var infowindow = new google.maps.InfoWindow({
-            content:"Come visit us! - J.Porrata, PSC"});
+            content:"Come visit us! - JLM Solutions Corp."});
         infowindow.open(map,marker);
     });
 }
